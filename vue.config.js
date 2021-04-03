d9f06d37cee55560faccd4e0e593998da041f60d
zhouzhi <Mr_zhouzhi@163.com>
2021-04-03 21:23:16 +0800

    Update vue.config.js
    fix

--- vue.config.js
+++ vue.config.js
@@ -95,7 +95,10 @@ module.exports = {
       //       overrideBrowserslist: ["last 15 versions"]
       //     }),
       //     require("postcss-pxtorem")({
-      //       rootValue: 75, // 换算的基数
+                  // 换算的基数 设计稿是多大宽度，直接/10 -->  1920 转化为 192 以此类推
+                  // 安装postcss-pxtorem  版本号需要注意 npm i postcss-pxtorem@5.1.1
+
+      //       rootValue: 75,
       //       // 忽略转换正则匹配项。插件会转化所有的样式的px。比如引入了三方UI，也会被转化。目前我使用 selectorBlackList字段，来过滤
       //       //如果个别地方不想转化px。可以简单的使用大写的 PX 或 Px 。
       //       selectorBlackList: ["el"],
